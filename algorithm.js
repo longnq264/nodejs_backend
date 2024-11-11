556aa4ff4570dbe169edfd277b85c34287f998e1
longnq264 <longhb2642002@gmail.com>
2024-11-11 22:22:16 +0700

    update sever

---
+++ algorithm.js
@@ -0,0 +1,22 @@
+const a = 1;
+
+console.log(a);
+
+// Buble Sort
+
+const listArr = [44, 3, 4, 3, 64, 12, 43, 65, 43, 87];
+
+const bubleSort = (array) => {
+
+    const setArr = new Set()
+
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] > 50) {
+      console.log(array[i]);
+    } else {
+      console.log(array[i]);
+    }
+  }
+};
+
+bubleSort(listArr);
--- app.js
+++ app.js
@@ -1,11 +1,38 @@
-const express = require("express")
+import express from "express";
+import cors from "cors";
+import formidable from "formidable";
+
 const app = express();
-const port = 3000;
+const port = 3030;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({
+    message: "Hello world",
+  });
+});
+
+app.post("/api/upload", (req, res, next) => {
+  const form = formidable({});
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json({ fields, files });
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port http://localhost:${port}/`);
+});
+
+
+
+const data = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
+
+data.forEach(item => {
+  const listItem = item
 });
